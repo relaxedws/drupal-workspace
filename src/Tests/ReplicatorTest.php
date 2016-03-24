f934e7693f84a9933efd73e5a5f78109c93a0fe8
Tim Millwood <tim@millwoodonline.co.uk>
2016-03-24 11:37:53 +0000

    making replicator test use correct workspaces and pointers

--- src/Tests/ReplicatorTest.php
+++ src/Tests/ReplicatorTest.php
@@ -8,6 +8,7 @@
 namespace Drupal\workspace\Tests;
 
 use Drupal\multiversion\Entity\Workspace;
+use Drupal\multiversion\Entity\WorkspaceInterface;
 use Drupal\node\Entity\Node;
 use Drupal\replication\Entity\ReplicationLogInterface;
 use Drupal\simpletest\WebTestBase;
@@ -48,13 +49,25 @@ class ReplicatorTest extends WebTestBase {
 
   public function testReplicator() {
     $this->assertEqual(count($this->node->get('workspace')), 1, 'Node is in one workspace');
+    $default_workspace_id = \Drupal::getContainer()->getParameter('workspace.default');
+    $default_workspace = Workspace::load($default_workspace_id);
+    $source_pointer = $this->loadPointer($this->developmentWorkspace);
+    $target_pointer = $this->loadPointer($default_workspace);
     /** @var \Drupal\replication\Entity\ReplicationLogInterface $replication_log */
-    $replication_log = \Drupal::service('workspace.replicator_manager')->replicate(WorkspacePointer::load(2), WorkspacePointer::load(1));
+    $replication_log = \Drupal::service('workspace.replicator_manager')->replicate($source_pointer, $target_pointer);
     $reloaded_node = Node::load($this->node->id());
     $this->assertEqual(count($reloaded_node->get('workspace')), 2, 'Node is in two workspaces');
     $this->assertTrue(($replication_log instanceof ReplicationLogInterface), "ReplicationLog returned.");
     $this->assertTrue($replication_log->get('ok'), "Replication went ok");
-    $this->assertEqual(3, $replication_log->getHistory()[0]['docs_read'], "ReplicationLog states 1 document was read");
-    $this->assertEqual(3, $replication_log->getHistory()[0]['docs_written'], "ReplicationLog states 1 document was written");
+    $this->assertEqual(1, $replication_log->getHistory()[0]['docs_read'], "ReplicationLog states 1 document was read");
+    $this->assertEqual(1, $replication_log->getHistory()[0]['docs_written'], "ReplicationLog states 1 document was written");
+  }
+
+  protected function loadPointer(WorkspaceInterface $workspace) {
+    $pointers = \Drupal::entityTypeManager()
+      ->getStorage('workspace_pointer')
+      ->loadByProperties(['workspace_pointer' => $workspace->id()]);
+    $pointer = reset($pointers);
+    return $pointer;
   }
 }
