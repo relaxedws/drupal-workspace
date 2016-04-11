d2f358d01657758d176202e059f5b27406d28cfc
Tim Millwood <tim@millwoodonline.co.uk>
2016-04-11 13:04:09 +0100

    Converting WorkspaceSwitcherTest to BrowserTestBase

---
+++ tests/src/Functional/WorkspaceSwitcherTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\workspace\Functional\WorkspaceSwitcherTest.
+ */
+
+namespace Drupal\Tests\workspace\Functional;
+
+use Drupal\multiversion\Entity\Workspace;
+use Drupal\simpletest\BlockCreationTrait;
+use Drupal\simpletest\BrowserTestBase;
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests workspace switching functionality.
+ *
+ * @group workspace
+ */
+class WorkspaceSwitcherTest extends BrowserTestBase {
+  use WorkspaceTestUtilities;
+  use BlockCreationTrait {
+    placeBlock as drupalPlaceBlock;
+  }
+
+  public static $modules = ['block', 'workspace'];
+
+  /**
+   * Test that the block displays and switches workspaces.
+   * Then test the admin page displays workspaces and allows switching.
+   */
+  public function testSwitchingWorkspaces() {
+    $permissions = [
+      'create_workspace',
+      'edit_own_workspace',
+      'view_own_workspace',
+      'bypass_entity_access_own_workspace',
+    ];
+
+    $this->setupWorkspaceSwitcherBlock();
+
+    $mayer = $this->drupalCreateUser($permissions);
+    $this->drupalLogin($mayer);
+
+    $vultures = $this->createWorkspaceThroughUI('Vultures', 'vultures');
+    $this->switchToWorkspace($vultures);
+
+    $gravity = $this->createWorkspaceThroughUI('Gravity', 'gravity');
+
+    $this->drupalGet('/admin/structure/workspace/' . $gravity->id() . '/activate');
+
+    $session = $this->getSession();
+    $this->assertEquals(200, $session->getStatusCode());
+    $page = $session->getPage();
+    $page->findButton(t('Activate'))->click();
+
+    $session->getPage()->findLink($gravity->label());
+
+  }
+}
