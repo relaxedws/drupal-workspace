ec60a006ca93a3dcea59e570a8e5ac63cf87356d
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-05-25 18:42:05 -0500

    Fix code style in ReplicatorManager

--- src/ReplicatorInterface.php
+++ src/ReplicatorInterface.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\workspace\ReplicatorInterface.
- */
-
 namespace Drupal\workspace;
 
 /**
@@ -13,16 +8,30 @@ namespace Drupal\workspace;
 interface ReplicatorInterface {
 
   /**
+   * Determine if the replicator applies to the given workspaces.
+   *
    * @param \Drupal\workspace\WorkspacePointerInterface $source
+   *   The workspace to replicate from.
    * @param \Drupal\workspace\WorkspacePointerInterface $target
+   *   The workspace to replicate to.
+   *
    * @return bool
+   *   Returns true if this replicator applies.
    */
   public function applies(WorkspacePointerInterface $source, WorkspacePointerInterface $target);
 
   /**
+   * Perform the replication from the source to target workspace.
+   *
    * @param \Drupal\workspace\WorkspacePointerInterface $source
+   *   The workspace to replicate from.
    * @param \Drupal\workspace\WorkspacePointerInterface $target
+   *   The workspace to replicate to.
+   *
    * @return array
+   *   The array of ??
+   *
+   * @todo document what the return actually is
    */
   public function replicate(WorkspacePointerInterface $source, WorkspacePointerInterface $target);
 
