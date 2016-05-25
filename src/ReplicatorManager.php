fa82ec40d5ef054fc07c4cb0fa40badb05940a95
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-05-25 18:43:10 -0500

    Fix code style in ReplicatorManager

--- src/ReplicatorManager.php
+++ src/ReplicatorManager.php
@@ -1,11 +1,7 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\workspace\ReplicatorManager.
- */
-
 namespace Drupal\workspace;
+
 use Drupal\multiversion\Workspace\ConflictTrackerInterface;
 use Drupal\replication\Entity\ReplicationLog;
 
@@ -14,12 +10,22 @@ use Drupal\replication\Entity\ReplicationLog;
  */
 class ReplicatorManager implements ReplicatorInterface {
 
-  /** @var ReplicatorInterface[] */
+  /**
+   * @var ReplicatorInterface[]
+   *   The services available to perform replication.
+   */
   protected $replicators = [];
 
-  /** @var  ConflictTrackerInterface */
+  /**
+   * @var ConflictTrackerInterface
+   *   The injected service to track conflicts during replication.
+   */
   protected $conflictTracker;
 
+  /**
+   * @param ConflictTrackerInterface $conflict_tracker
+   *   The injected service to track conflicts during replication.
+   */
   public function __construct(ConflictTrackerInterface $conflict_tracker) {
     $this->conflictTracker = $conflict_tracker;
   }
@@ -35,6 +41,7 @@ class ReplicatorManager implements ReplicatorInterface {
    * Adds replication services.
    *
    * @param ReplicatorInterface $replicator
+   *   The service to make available for performing replication.
    */
   public function addReplicator(ReplicatorInterface $replicator) {
     $this->replicators[] = $replicator;
@@ -44,17 +51,34 @@ class ReplicatorManager implements ReplicatorInterface {
    * {@inheritdoc}
    */
   public function replicate(WorkspacePointerInterface $source, WorkspacePointerInterface $target) {
+    // @todo why is $initial_conflicts not used?
     $initial_conflicts = $this->conflictTracker->getAll();
+    // @todo why is $pull unused?
     $pull = $this->update($target, $source);
+    // @todo why is $post_conflicts unused?
     $post_conflicts = $this->conflictTracker->getAll();
     $push = $this->doReplication($source, $target);
     return $push;
   }
 
+  /**
+   * @todo document what this is for
+   */
   public function update(WorkspacePointerInterface $source, WorkspacePointerInterface $target) {
     return $this->doReplication($source, $target);
   }
 
+  /**
+   * Internal method to contain replication logic.
+   *
+   * @param \Drupal\workspace\WorkspacePointerInterface $source
+   *   The workspace to replicate from.
+   * @param \Drupal\workspace\WorkspacePointerInterface $target
+   *   The workspace to replicate to.
+   *
+   * @return ReplicationLog
+   *   The log entry for this replication.
+   */
   protected function doReplication(WorkspacePointerInterface $source, WorkspacePointerInterface $target) {
     foreach ($this->replicators as $replicator) {
       if ($replicator->applies($source, $target)) {
@@ -65,12 +89,23 @@ class ReplicatorManager implements ReplicatorInterface {
     return $this->failedReplicationLog($source, $target);
   }
 
+  /**
+   * Generate a failed replication log and return it.
+   *
+   * @param \Drupal\workspace\WorkspacePointerInterface $source
+   *   The workspace to replicate from.
+   * @param \Drupal\workspace\WorkspacePointerInterface $target
+   *   The workspace to replicate to.
+   *
+   * @return ReplicationLog
+   *   The log entry for this replication.
+   */
   protected function failedReplicationLog(WorkspacePointerInterface $source, WorkspacePointerInterface $target) {
     $time = new \DateTime();
     $history = [
       'start_time' => $time->format('D, d M Y H:i:s e'),
       'end_time' => $time->format('D, d M Y H:i:s e'),
-      'session_id' => \md5((\microtime(true) * 1000000)),
+      'session_id' => \md5((\microtime(TRUE) * 1000000)),
     ];
     $replication_log_id = $source->generateReplicationId($target);
     /** @var \Drupal\replication\Entity\ReplicationLogInterface $replication_log */
@@ -81,4 +116,5 @@ class ReplicatorManager implements ReplicatorInterface {
     $replication_log->save();
     return $replication_log;
   }
+
 }
