c0022f1d47f847152e4e711e2d0265bb3e9a1e68
Joseph D. Purcell <josephdpurcell@gmail.com>
2016-06-01 15:45:35 -0500

    Add task to replicator interface

--- src/ReplicatorInterface.php
+++ src/ReplicatorInterface.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\workspace;
 
+use Drupal\replication\ReplicationTask\ReplicationTaskInterface;
+
 /**
  * Defines an interface for Replicator plugins.
  */
@@ -17,6 +19,8 @@ interface ReplicatorInterface {
    *
    * @return bool
    *   Returns true if this replicator applies.
+   *
+   * @todo should ReplicationTaskInterface be passed here as well?
    */
   public function applies(WorkspacePointerInterface $source, WorkspacePointerInterface $target);
 
@@ -27,12 +31,12 @@ interface ReplicatorInterface {
    *   The workspace to replicate from.
    * @param \Drupal\workspace\WorkspacePointerInterface $target
    *   The workspace to replicate to.
+   * @param \Drupal\replication\ReplicationTask\ReplicationTaskInterface $task
+   *   Optional information that defines the replication task to perform.
    *
-   * @return array
-   *   The array of ??
-   *
-   * @todo document what the return actually is
+   * @return \Drupal\replication\Entity\ReplicationLog
+   *   The replication log entry.
    */
-  public function replicate(WorkspacePointerInterface $source, WorkspacePointerInterface $target);
+  public function replicate(WorkspacePointerInterface $source, WorkspacePointerInterface $target, ReplicationTaskInterface $task);
 
 }
