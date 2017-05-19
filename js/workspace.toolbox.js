831a88543ca73fb8d6a9fcc347d03f4670df02d1
Tim Millwood <tim@millwoodonline.co.uk>
2017-05-19 19:46:55 +0100

    Initial implementation of workspace toolbox

---
+++ js/workspace.toolbox.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Drupal's Settings Tray library.
+ */
+
+(function ($, Drupal) {
+    Drupal.behaviors.workspaceToolbox = {
+        attach: function (context, settings) {
+            $(context).find('a.toolbar-icon-workspace').once('workspaceToolbox').click(function () {
+                $('body').toggleClass('transform-active').css('padding-top', 0);
+                $('div.toolbox').toggleClass('toolbox-active');
+                event.stopPropagation();
+                event.preventDefault();
+                return false;
+            });
+            $(context).find('a.toolbox-close').once('workspaceToolbox').click(function () {
+                $('body').toggleClass('transform-active').css('padding-top', '79px');
+                $('div.toolbox').toggleClass('toolbox-active');
+                event.stopPropagation();
+                event.preventDefault();
+                return false;
+            });
+        }
+    };
+})(jQuery, Drupal);
