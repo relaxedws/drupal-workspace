f6f2442d5078e6666a121f5b720ba55f79530b6b
Tim Millwood <tim@millwoodonline.co.uk>
2017-05-23 14:58:10 +0100

    toolbox updates

--- js/workspace.toolbox.js
+++ js/workspace.toolbox.js
@@ -6,14 +6,14 @@
 (function ($, Drupal) {
     Drupal.behaviors.workspaceToolbox = {
         attach: function (context, settings) {
-            $(context).find('a.toolbar-icon-workspace').once('workspaceToolbox').click(function () {
+            $(context).find('a.toolbar-icon-workspace').once('workspaceToolbox').click(function (event) {
                 $('body').toggleClass('transform-active').css('padding-top', 0);
                 $('div.toolbox').toggleClass('toolbox-active');
                 event.stopPropagation();
                 event.preventDefault();
                 return false;
             });
-            $(context).find('button.toolbox-close').once('workspaceToolbox').click(function () {
+            $(context).find('button.toolbox-close').once('workspaceToolbox').click(function (event) {
                 $('body').toggleClass('transform-active').css('padding-top', '79px');
                 $('div.toolbox').toggleClass('toolbox-active');
                 event.stopPropagation();
