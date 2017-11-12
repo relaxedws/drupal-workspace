523a4bcdc0c05e841432ab004f8fba4c259312e4
Andrei Mateescu <andrei@amateescu.me>
2017-11-12 02:57:27 +0200

    Issue #2922788 by amateescu: Fix the workspace cache context

--- src/WorkspaceCacheContext.php
+++ src/WorkspaceCacheContext.php
@@ -40,7 +40,7 @@ class WorkspaceCacheContext implements CacheContextInterface {
    * {@inheritdoc}
    */
   public function getContext() {
-    return 'ws.' . $this->workspaceManager->getActiveWorkspace();
+    return $this->workspaceManager->getActiveWorkspace();
   }
 
   /**
--- src/WorkspaceServiceProvider.php
+++ src/WorkspaceServiceProvider.php
@@ -6,7 +6,7 @@ use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 
 /**
- * Defines a service profiler for the workspace module.
+ * Defines a service provider for the workspace module.
  */
 class WorkspaceServiceProvider extends ServiceProviderBase {
 
@@ -14,6 +14,7 @@ class WorkspaceServiceProvider extends ServiceProviderBase {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
+    // Add the 'workspace' cache context as required.
     $renderer_config = $container->getParameter('renderer.config');
     $renderer_config['required_cache_contexts'][] = 'workspace';
     $container->setParameter('renderer.config', $renderer_config);
