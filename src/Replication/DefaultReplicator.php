c986f8cc0f4211f151dda772520d26e23adc4054
timmillwood <timmillwood@227849.no-reply.drupal.org>
2017-11-24 12:46:08 +0000

    Issue #2917628 by timmillwood, amateescu: After replication the revision should be the same on both workspaces

--- src/Replication/DefaultReplicator.php
+++ src/Replication/DefaultReplicator.php
@@ -2,12 +2,16 @@
 
 namespace Drupal\workspace\Replication;
 
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\workspace\Changes\ChangesFactoryInterface;
+use Drupal\workspace\Entity\ContentWorkspace;
 use Drupal\workspace\Entity\ReplicationLog;
+use Drupal\workspace\Entity\ReplicationLogInterface;
 use Drupal\workspace\Entity\Workspace;
 use Drupal\workspace\Index\SequenceIndexInterface;
 use Drupal\workspace\UpstreamPluginInterface;
+use Drupal\workspace\WorkspaceManager;
 use Drupal\workspace\WorkspaceManagerInterface;
 
 /**
@@ -47,6 +51,13 @@ class DefaultReplicator implements ReplicationInterface {
   protected $sequenceIndex;
 
   /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
    * Constructs the default replicator service.
    *
    * @param \Drupal\workspace\WorkspaceManagerInterface $workspace_manager
@@ -57,12 +68,15 @@ class DefaultReplicator implements ReplicationInterface {
    *   The entity type manager.
    * @param \Drupal\workspace\Index\SequenceIndexInterface $sequence_index
    *   The sequence index.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database connection.
    */
-  public function __construct(WorkspaceManagerInterface $workspace_manager, ChangesFactoryInterface $changes_factory, EntityTypeManagerInterface $entity_type_manager, SequenceIndexInterface $sequence_index) {
+  public function __construct(WorkspaceManagerInterface $workspace_manager, ChangesFactoryInterface $changes_factory, EntityTypeManagerInterface $entity_type_manager, SequenceIndexInterface $sequence_index, Connection $database) {
     $this->workspaceManager = $workspace_manager;
     $this->changesFactory = $changes_factory;
     $this->entityTypeManager = $entity_type_manager;
     $this->sequenceIndex = $sequence_index;
+    $this->database = $database;
   }
 
   /**
@@ -88,78 +102,168 @@ class DefaultReplicator implements ReplicationInterface {
     $source_workspace = Workspace::load($source->getDerivativeId());
     $target_workspace = Workspace::load($target->getDerivativeId());
     $start_time = new \DateTime();
-    $sessionId = \md5((\microtime(TRUE) * 1000000));
+    $session_id = \md5((\microtime(TRUE) * 1000000));
     // @todo Figure out if we want to include more information in the
     // replication log ID.
     // @see http://docs.couchdb.org/en/2.0.0/replication/protocol.html#generate-replication-id
     $replication_id = hash('sha256', $source_workspace->id() . $target_workspace->id());
+
+    // Load or create the Replication Log entity based on the replication ID.
     $replication_log = ReplicationLog::loadOrCreate($replication_id);
 
+    // Get the current active workspace, so we can set it back as the active
+    // after the replication has completed.
     $current_active = $this->workspaceManager->getActiveWorkspace(TRUE);
 
-    // Set the source as the active workspace.
+    // Set the source as the active workspace, so we can fetch all the entities
+    // relative to the source workspace.
     $this->workspaceManager->setActiveWorkspace($source_workspace);
 
-    // Get changes for the current workspace.
-    $history = $replication_log->getHistory();
-    $last_sequence_id = isset($history[0]['recorded_sequence']) ? $history[0]['recorded_sequence'] : 0;
-    $changes = $this->changesFactory->get($source_workspace)->setLastSequenceId($last_sequence_id)->getChanges();
-    $rev_diffs = [];
-    /** @var \Drupal\workspace\Changes\Change $change */
-    foreach ($changes as $change) {
-      $rev_diffs[$change->getEntityTypeId()][] = $change->getRevisionId();
+    // Get changes for the current workspace based on the sequence ID from the
+    // last replication between this workspaces, if there is one.
+    $changes = $this->changesFactory->get($source_workspace)->setLastSequenceId($this->getLastSequenceId($replication_log))->getChanges();
+
+    // If there are no changes then there's no need to continue with the
+    // replication.
+    if (empty($changes)) {
+      return $this->updateReplicationLog($replication_log, [
+        'entity_write_failures' => 0,
+        'entities_read' => 0,
+        'entities_written' => 0,
+        'end_last_sequence' => $this->sequenceIndex->useWorkspace($source_workspace->id())->getLastSequenceId(),
+        'end_time' => (new \DateTime())->format('D, d M Y H:i:s e'),
+        'recorded_sequence' => $this->sequenceIndex->useWorkspace($source_workspace->id())->getLastSequenceId(),
+        'session_id' => $session_id,
+        'start_last_sequence' => $this->getLastSequenceId($replication_log),
+        'start_time' => $start_time->format('D, d M Y H:i:s e'),
+      ]);
     }
 
-    // Get revision diff between source and target
+    // Reduce the changes to an array of revision IDs, keyed by the entity type
+    // ID.
+    $changes = array_reduce($changes, function ($reduced, $change) {
+      /** @var \Drupal\workspace\Changes\Change $change */
+      $reduced[$change->getEntityTypeId()][] = $change->getRevisionId();
+      return $reduced;
+    });
+
     $content_workspace_ids = [];
-    foreach ($rev_diffs as $entity_type_id => $revs) {
+    foreach ($changes as $entity_type_id => $revision_ids) {
+      // Get all entity revision IDs for all entities which are in only one
+      // of either the source or the target workspaces. We assume that this
+      // means the revision is in the source, but not the target, and the
+      // revision has not been replicated yet.
+      $select = $this->database
+        ->select('content_workspace_field_revision', 'cwfr')
+        ->fields('cwfr', ['content_entity_revision_id']);
+      $select->condition('content_entity_type_id', $entity_type_id);
+      $select->condition('content_entity_revision_id', $revision_ids, 'IN');
+      $select->condition('workspace', [$source_workspace->id(), $target_workspace->id()], 'IN');
+      $select->groupBy('content_entity_revision_id');
+      $select->having('count(workspace) < :workspaces', [':workspaces' => 2]);
+      $revision_difference = $select->execute()->fetchCol();
+
+      // Get the content workspace IDs for all of the entity revision IDs which
+      // are not yet in the target workspace.
       $content_workspace_ids[$entity_type_id] = $this->entityTypeManager
         ->getStorage('content_workspace')
         ->getQuery()
         ->allRevisions()
         ->condition('content_entity_type_id', $entity_type_id)
-        ->condition('content_entity_revision_id', $revs, 'IN')
-        ->condition('workspace', $target_workspace->id())
+        ->condition('content_entity_revision_id', $revision_difference, 'IN')
+        ->condition('workspace', $source_workspace->id())
         ->execute();
     }
+
+    $entities = [];
     foreach ($content_workspace_ids as $entity_type_id => $ids) {
-      foreach ($ids as $id) {
-        $key = array_search($id, $rev_diffs[$entity_type_id]);
-        if (isset($key)) {
-          unset($rev_diffs[$entity_type_id][$key]);
+      foreach ($ids as $revision_id => $entity_id) {
+        // Get the content workspace entity for revision that is in the source
+        // workspace.
+        /** @var \Drupal\workspace\Entity\ContentWorkspaceInterface $content_workspace */
+        $content_workspace = $this->entityTypeManager->getStorage('content_workspace')->loadRevision($revision_id);
+        if (WorkspaceManager::DEFAULT_WORKSPACE === $target_workspace->id()) {
+          // If the target workspace is the default workspace (generally 'live')
+          // the revision needs to be set to the default revision.
+          /** @var \Drupal\Core\Entity\ContentEntityInterface|\Drupal\Core\Entity\RevisionableInterface $entity */
+          $entity = $this->entityTypeManager
+            ->getStorage($content_workspace->content_entity_type_id->value)
+            ->loadRevision($content_workspace->content_entity_revision_id->value);
+          $entity->_isReplicating = TRUE;
+          $entity->isDefaultRevision(TRUE);
+          $entities[] = $entity;
+        }
+        else {
+          // If the target workspace is not a default workspace the content
+          // workspace link entity can simply be updated with the target
+          // workspace.
+          $content_workspace->setNewRevision(TRUE);
+          $content_workspace->workspace->target_id = $target_workspace->id();
+          // Use the updateOrCreateFormEntity() method to make sure the content
+          // entity is not updated.
+          // @todo: Look into if we need this method at all.
+          ContentWorkspace::updateOrCreateFromEntity($content_workspace);
         }
       }
     }
 
-    $entities = [];
-    // Load each missing revision.
-    foreach ($rev_diffs as $entity_type_id => $revs) {
-      foreach ($revs as $rev) {
-        /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-        $entity = $this->entityTypeManager
-          ->getStorage($entity_type_id)
-          ->loadRevision($rev);
-        $entity->isDefaultRevision(TRUE);
-        $entities[] = $entity;
+    // Only switch to the target workspace and save entities if there are some
+    // to save.
+    if (!empty($entities)) {
+      // Before saving set the active workspace to the target.
+      $this->workspaceManager->setActiveWorkspace($target_workspace);
+      // Save each revision on the target workspace.
+      foreach ($entities as $entity) {
+        $entity->save();
       }
     }
 
-    // Before saving set the active workspace to the target.
-    $this->workspaceManager->setActiveWorkspace($target_workspace);
-
-    // Save each revision on the target workspace
-    foreach ($entities as $entity) {
-      $entity->save();
-    }
-
-    // Log
+    // Switch back to the original active workspace, so that the user performing
+    // the replication is back on the workspace they started on.
     $this->workspaceManager->setActiveWorkspace($current_active);
 
-    $replication_log->setHistory([
-      'recorded_sequence' => $this->sequenceIndex->useWorkspace($source_workspace->id())->getLastSequenceId(),
+    // Update the replication log entity by adding the completed replication to
+    // the history.
+    return $this->updateReplicationLog($replication_log, [
+      'entity_write_failures' => 0,
+      'entities_read' => count($changes),
+      'entities_written' => count($revision_difference),
+      'end_last_sequence' => $this->sequenceIndex->useWorkspace($target_workspace->id())->getLastSequenceId(),
+      'end_time' => (new \DateTime())->format('D, d M Y H:i:s e'),
+      'recorded_sequence' => $this->sequenceIndex->useWorkspace($target_workspace->id())->getLastSequenceId(),
+      'session_id' => $session_id,
+      'start_last_sequence' => $this->getLastSequenceId($replication_log),
       'start_time' => $start_time->format('D, d M Y H:i:s e'),
-      'session_id' => $sessionId,
     ]);
+  }
+
+  /**
+   * Gets the last sequence ID from a replication log entity.
+   *
+   * @param \Drupal\workspace\Entity\ReplicationLogInterface $replication_log
+   *   The replication log entity to get the last sequence ID from.
+   *
+   * @return int
+   *   The last sequence ID for the replication log.
+   */
+  protected function getLastSequenceId(ReplicationLogInterface $replication_log) {
+    $history = $replication_log->getHistory();
+    return isset($history[0]['recorded_sequence']) ? $history[0]['recorded_sequence'] : 0;
+  }
+
+  /**
+   * Updates the replication log entity with the given history.
+   *
+   * @param \Drupal\workspace\Entity\ReplicationLogInterface $replication_log
+   *   The replication log entity to be updated.
+   * @param array $history
+   *   The new history items.
+   *
+   * @return \Drupal\workspace\Entity\ReplicationLogInterface
+   *   The updated replication log entity.
+   */
+  protected function updateReplicationLog(ReplicationLogInterface $replication_log, array $history) {
+    $replication_log->setHistory($history);
     $replication_log->save();
     return $replication_log;
   }
--- tests/src/Functional/ReplicationTest.php
+++ tests/src/Functional/ReplicationTest.php
@@ -2,11 +2,12 @@
 
 namespace Drupal\Tests\workspace\Functional;
 
-use Drupal\simpletest\BlockCreationTrait;
+use Drupal\Tests\block\Traits\BlockCreationTrait;
 use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\node\Traits\NodeCreationTrait;
 
 /**
- * Class ReplicationTest
+ * Tests replication of entities between workspaces.
  *
  * @group workspace
  */
@@ -17,6 +18,8 @@ class ReplicationTest extends BrowserTestBase {
   use BlockCreationTrait {
     placeBlock as drupalPlaceBlock;
   }
+
+  use NodeCreationTrait;
 
   public static $modules = [
     'system',
@@ -60,48 +63,51 @@ class ReplicationTest extends BrowserTestBase {
     $this->setupWorkspaceSwitcherBlock();
   }
 
+  /**
+   * Tests replicating Nodes between workspaces.
+   */
   public function testNodeReplication() {
+    // Create dev, get stage and live, set dev to the active workspace.
     $dev = $this->createWorkspaceThroughUI('Dev', 'dev', 'local_workspace:stage');
     $stage = $this->getOneWorkspaceByLabel('Stage');
     $live = $this->getOneWorkspaceByLabel('Live');
     $this->switchToWorkspace($dev);
 
-    $this->drupalGet('/node/add/test');
-    $session = $this->getSession();
-    $this->assertEquals(200, $session->getStatusCode());
-    $page = $session->getPage();
-    $page->fillField('Title', 'Test node');
-    $page->findButton(t('Save'))->click();
-    $page = $session->getPage();
-    $this->assertTrue($page->hasContent("Test node has been created"));
+    // Add a test node.
+    $this->createNode(['type' => 'test', 'title' => 'Test node']);
+    // Made an edit to the test node.
     $this->drupalGet('/node/1/edit');
     $session = $this->getSession();
     $this->assertEquals(200, $session->getStatusCode());
-    $page->findButton(t('Save'))->click();
+    $session->getPage()->findButton(t('Save'))->click();
 
-    $this->assertEquals($dev->id(), $this->getOneEntityByLabel('node', 'Test node')->workspace->target_id);
+    // Loading the node should get revision 2 on the dev workspace.
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(2, $entity->getRevisionId());
+    $this->assertEquals($dev->id(), $entity->workspace->target_id);
 
     $this->drupalGet('/admin/config/workflow/workspace/' . $dev->id() . '/deploy');
     $session = $this->getSession();
-    $page = $session->getPage();
     $this->assertEquals(200, $session->getStatusCode());
-    $page->findButton('edit-submit')->click();
+    $session->getPage()->findButton('edit-submit')->click();
     $session->getPage()->hasContent('Successful deployment');
 
+    // Switch to stage and the node will still be revision 2, but now on the
+    // stage workspace.
     $this->switchToWorkspace($stage);
-    $this->assertEquals($stage->id(), $this->getOneEntityByLabel('node', 'Test node')->workspace->target_id);
+    $entity = $this->getOneEntityByLabel('node', 'Test node');
+    $this->assertEquals(2, $entity->getRevisionId());
+    $this->assertEquals($stage->id(), $entity->workspace->getValue()[0]['target_id']);
 
-    $this->drupalGet('/node/add/test');
-    $session = $this->getSession();
-    $this->assertEquals(200, $session->getStatusCode());
-    $page = $session->getPage();
-    $page->fillField('Title', 'Test stage node');
-    $page->findButton(t('Save'))->click();
-    $page = $session->getPage();
-    $page->hasContent("Test stage node has been created");
+    // Add a test node on the stage workspace.
+    $this->createNode(['type' => 'test', 'title' => 'Test stage node']);
 
-    $this->assertEquals($stage->id(), $this->getOneEntityByLabel('node', 'Test stage node')->workspace->target_id);
+    // Loading the node should get revision 3 on the stage workspace.
+    $entity2 = $this->getNodeByTitle('Test stage node');
+    $this->assertEquals(3, $entity2->getRevisionId());
+    $this->assertEquals($stage->id(), $entity2->workspace->target_id);
 
+    // Run a deployment from stage to live.
     $this->drupalGet('/admin/config/workflow/workspace/' . $stage->id() . '/deploy');
     $session = $this->getSession();
     $page = $session->getPage();
@@ -109,9 +115,91 @@ class ReplicationTest extends BrowserTestBase {
     $page->findButton('edit-submit')->click();
     $session->getPage()->hasContent('Successful deployment');
 
+    // Switch to the live workspace and load both nodes. They should both have
+    // the original revision IDs and be on the live workspace.
     $this->switchToWorkspace($live);
-    $this->assertEquals($live->id(), $this->getOneEntityByLabel('node', 'Test node')->workspace->target_id);
-    $this->assertEquals($live->id(), $this->getOneEntityByLabel('node', 'Test stage node')->workspace->target_id);
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(2, $entity->getRevisionId());
+    $entity2 = $this->getNodeByTitle('Test stage node');
+    $this->assertEquals(3, $entity2->getRevisionId());
+    $this->assertEquals($live->id(), $entity->workspace->target_id);
+    $this->assertEquals($live->id(), $entity2->workspace->target_id);
+
+    // Switch back to dev and update 'Test node'.
+    $this->switchToWorkspace($dev);
+    // Made an edit to the test node.
+    $this->drupalGet('/node/1/edit');
+    $session = $this->getSession();
+    $this->assertEquals(200, $session->getStatusCode());
+    $session->getPage()->findButton(t('Save'))->click();
+
+    // Loading the node should get revision 4 on the dev workspace.
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(4, $entity->getRevisionId());
+    $this->assertEquals($dev->id(), $entity->workspace->target_id);
+
+    // Loading the node should get revision 2 on the stage workspace.
+    $this->switchToWorkspace($stage);
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(2, $entity->getRevisionId());
+    $this->assertEquals($stage->id(), $entity->get('workspace')->getValue()[1]['target_id']);
+
+    // Loading the node should get revision 2 on the live workspace.
+    $this->switchToWorkspace($live);
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(2, $entity->getRevisionId());
+    $this->assertEquals($live->id(), $entity->workspace->target_id);
+
+    // Run deployment from dev to stage.
+    $this->switchToWorkspace($dev);
+    $this->drupalGet('/admin/config/workflow/workspace/' . $dev->id() . '/deploy');
+    $session = $this->getSession();
+    $this->assertEquals(200, $session->getStatusCode());
+    $session->getPage()->findButton('edit-submit')->click();
+    $session->getPage()->hasContent('Successful deployment');
+
+    // Loading the node should get revision 4 on the dev workspace.
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(4, $entity->getRevisionId());
+    $this->assertEquals($dev->id(), $entity->get('workspace')->getValue()[1]['target_id']);
+
+    // Loading the node should get revision 4 on the stage workspace.
+    $this->switchToWorkspace($stage);
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(4, $entity->getRevisionId());
+    $this->assertEquals($stage->id(), $entity->get('workspace')->getValue()[0]['target_id']);
+
+    // Loading the node should get revision 2 on the live workspace.
+    $this->switchToWorkspace($live);
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(2, $entity->getRevisionId());
+    $this->assertEquals($live->id(), $entity->workspace->target_id);
+
+    // Run deployment from stage to live.
+    $this->switchToWorkspace($stage);
+    $this->drupalGet('/admin/config/workflow/workspace/' . $stage->id() . '/deploy');
+    $session = $this->getSession();
+    $this->assertEquals(200, $session->getStatusCode());
+
+    $session->getPage()->findButton('edit-submit')->click();
+    $session->getPage()->hasContent('Successful deployment');
+
+    // Loading the node should get revision 4 on the dev workspace.
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(4, $entity->getRevisionId());
+    $this->assertEquals($dev->id(), $entity->get('workspace')->getValue()[2]['target_id']);
+
+    // Loading the node should get revision 4 on the stage workspace.
+    $this->switchToWorkspace($stage);
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(4, $entity->getRevisionId());
+    $this->assertEquals($stage->id(), $entity->get('workspace')->getValue()[1]['target_id']);
+
+    // Loading the node should get revision 4 on the live workspace.
+    $this->switchToWorkspace($live);
+    $entity = $this->getNodeByTitle('Test node');
+    $this->assertEquals(4, $entity->getRevisionId());
+    $this->assertEquals($live->id(), $entity->workspace->target_id);
   }
 
 }
