073c6385fa6bc8f6fe2c8603437f1b59326704fd
Andrei Jechiu <andrei.jechiu@gmail.com>
2017-11-28 16:21:55 +0200

    Use revision differences to get changes between local and remote workspaces.

--- src/Controller/ChangesListController.php
+++ src/Controller/ChangesListController.php
@@ -4,12 +4,16 @@ namespace Drupal\workspace\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\multiversion\Entity\Index\RevisionIndexInterface;
+use Drupal\multiversion\Entity\WorkspaceInterface;
 use Drupal\multiversion\Workspace\WorkspaceManagerInterface;
 use Drupal\replication\ChangesFactoryInterface;
+use Drupal\replication\ReplicationTask\ReplicationTask;
 use Drupal\replication\RevisionDiffFactoryInterface;
+use LogicException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ChangesListController extends ControllerBase {
 
@@ -34,18 +38,25 @@ class ChangesListController extends ControllerBase {
   protected $revIndex;
 
   /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * ChangesListController constructor.
    *
    * @param \Drupal\multiversion\Workspace\WorkspaceManagerInterface $workspace_manager
    * @param \Drupal\replication\ChangesFactoryInterface $changes_factory
    * @param \Drupal\replication\RevisionDiffFactoryInterface $revisiondiff_factory
    * @param \Drupal\multiversion\Entity\Index\RevisionIndexInterface $rev_index
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    */
-  public function __construct(WorkspaceManagerInterface $workspace_manager, ChangesFactoryInterface $changes_factory, RevisionDiffFactoryInterface $revisiondiff_factory, RevisionIndexInterface $rev_index) {
+  public function __construct(WorkspaceManagerInterface $workspace_manager, ChangesFactoryInterface $changes_factory, RevisionDiffFactoryInterface $revisiondiff_factory, RevisionIndexInterface $rev_index, EntityTypeManagerInterface $entity_type_manager) {
     $this->workspaceManager = $workspace_manager;
     $this->changesFactory = $changes_factory;
     $this->revisionDiffFactory = $revisiondiff_factory;
     $this->revIndex = $rev_index;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -56,7 +67,8 @@ class ChangesListController extends ControllerBase {
       $container->get('workspace.manager'),
       $container->get('replication.changes_factory'),
       $container->get('replication.revisiondiff_factory'),
-      $container->get('multiversion.entity_index.rev')
+      $container->get('multiversion.entity_index.rev'),
+      $container->get('entity_type.manager')
     );
   }
 
@@ -70,29 +82,34 @@ class ChangesListController extends ControllerBase {
    */
   public function viewChanges($workspace) {
     $source_workspace = $this->workspaceManager->load($workspace);
-    if ($source_workspace === NULL) {
-      throw new NotFoundHttpException();
-    }
+    $source_workspace_pointer = $this->getPointerToWorkspace($source_workspace);
     $target_workspace_pointer = $source_workspace->get('upstream')->entity;
     if (!$target_workspace_pointer) {
-      return ['#content' => $this->t('The target workspace is not set for the @label workspace.', ['@label' => $workspace])];
+      return ['#content' => $this->t('The upstream is not set.')];
     }
-    $entities = $this->getChanges($source_workspace);
+    $target_workspace = $target_workspace_pointer->getWorkspace();
+    if ($target_workspace instanceof WorkspaceInterface) {
+      $entities = $this->getChangesBetweenLocalWorkspaces($source_workspace, $target_workspace);
+    }
+    else {
+      $entities = $this->getChangesBetweenRemoteWorkspaces($source_workspace_pointer, $target_workspace_pointer);
+    }
 
     return $this->adminOverview($entities);
   }
 
   /**
-   * Return the array with changed entities on a specific workspace.
+   * Return the array with changed entities for workspaces on the same site.
    *
    * @param $source_workspace
+   * @param $target_workspace
    *
    * @return array
    */
-  protected function getChanges($source_workspace) {
-    $workspace_id = $source_workspace->id();
-    $since = \Drupal::state()->get('last_sequence.workspace.' . $workspace_id, 0);
-    // Get all changes on the source workspace starting with last sequence.
+  protected function getChangesBetweenLocalWorkspaces($source_workspace, $target_workspace) {
+    // @todo Check if it's possible to get the since value from replication log.
+    $since = \Drupal::state()->get('last_sequence.workspace.' . $source_workspace->id(), 0);
+    // Get changes on the source workspace.
     $source_changes = $this->changesFactory->get($source_workspace)
       ->setSince($since)
       ->getNormal();
@@ -104,15 +121,74 @@ class ChangesListController extends ControllerBase {
       }
     }
 
-    // Get new revisions on the source workspace.
+    // Get revisions the target workspace is missing.
+    $revs_diff = $this->revisionDiffFactory->get($target_workspace)->setRevisionIds($data)->getMissing();
+    return $this->getEntitiesFromRevsDiff($source_workspace, $revs_diff);
+  }
+
+  /**
+   * Return the array with changed entities when target is a remote workspace.
+   *
+   * @param $source_workspace_pointer
+   * @param $target_workspace_pointer
+   *
+   * @return array
+   */
+  protected function getChangesBetweenRemoteWorkspaces($source_workspace_pointer, $target_workspace_pointer) {
+    $since = \Drupal::state()->get('last_sequence.workspace.' . $source_workspace_pointer->getWorkspaceId(), 0);
+    $replicator = \Drupal::service('relaxed.couchdb_replicator');
+    $source = $replicator->setupEndpoint($source_workspace_pointer);
+    $target = $replicator->setupEndpoint($target_workspace_pointer);
+    $revs_diff = [];
+    $source_workspace = $source_workspace_pointer->getWorkspace();
+    $task = $this->getTask($source_workspace, 'push_replication_settings');
+    while (1) {
+      $changes = $source->getChanges(
+        array(
+          'feed' => 'normal',
+          'style' => 'all_docs',
+          'since' => $since,
+          'filter' => $task->getFilter(),
+          'parameters' => $task->getParameters(),
+          'doc_ids' => NULL,
+          'limit' => 100,
+        )
+      );
+      if (empty($changes['results']) || empty($changes['last_seq'])) {
+        break;
+      }
+      $data = [];
+      foreach (array_reverse($changes['results']) as $source_change) {
+        $data[$source_change['id']] = [];
+        foreach ($source_change['changes'] as $change) {
+          $data[$source_change['id']][] = $change['rev'];
+        }
+      }
+      $revs_diff += !empty($data) ? $target->getRevisionDifference($data) : [];
+      if (!in_array($changes['last_seq'], array_column($changes['results'], 'seq'))) {
+        break;
+      }
+      $since = $changes['last_seq'];
+    }
+
+    return $this->getEntitiesFromRevsDiff($source_workspace, $revs_diff);
+  }
+
+  /**
+   * @param $source_workspace \Drupal\multiversion\Entity\Workspace
+   * @param $revs_diff array
+   *
+   * @return array
+   */
+  protected function getEntitiesFromRevsDiff($source_workspace, $revs_diff) {
     $entities = [];
-    foreach ($data as $uuid => $revs) {
-      foreach ($revs as $rev) {
-        $item = $this->revIndex->useWorkspace($workspace_id)->get("$uuid:$rev");
+    foreach ($revs_diff as $uuid => $revs) {
+      foreach ($revs['missing'] as $rev) {
+        $item = $this->revIndex->useWorkspace($source_workspace->id())->get("$uuid:$rev");
         $entity_type_id = $item['entity_type_id'];
         $revision_id = $item['revision_id'];
 
-        $storage = $this->entityTypeManager()->getStorage($entity_type_id)->useWorkspace($workspace_id);
+        $storage = $this->entityTypeManager()->getStorage($entity_type_id);
         $entity = $storage->loadRevision($revision_id);
         if ($entity instanceof ContentEntityInterface) {
           $entities[] = $entity;
@@ -202,6 +278,64 @@ class ChangesListController extends ControllerBase {
         '@target' => $target_workspace_label
       ]
     );
+  }
+
+  /**
+   * Returns a pointer to the specified workspace.
+   *
+   * In most cases this pointer will be unique, but that is not guaranteed
+   * by the schema. If there are multiple pointers, which one is returned is
+   * undefined.
+   *
+   * @param \Drupal\multiversion\Entity\WorkspaceInterface $workspace
+   *   The workspace for which we want a pointer.
+   *
+   * @return \Drupal\workspace\WorkspacePointerInterface
+   *   The pointer to the provided workspace.
+   */
+  protected function getPointerToWorkspace(WorkspaceInterface $workspace) {
+    $pointers = $this->entityTypeManager
+      ->getStorage('workspace_pointer')
+      ->loadByProperties(['workspace_pointer' => $workspace->id()]);
+    $pointer = reset($pointers);
+    return $pointer;
+  }
+
+  protected function getMapping($changes)
+  {
+    $rows = array();
+    $arr = \explode("\n",$changes);
+    foreach ($arr as $line) {
+      if (\strlen($line) > 0) {
+        $rows[] = json_decode($line, true);
+      }
+    }
+    // To be sent to target/_revs_diff.
+    $mapping = array();
+    foreach ($rows as $row) {
+      $mapping[$row['id']] = array();
+      foreach ($row['changes'] as $revision) {
+        $mapping[$row['id']][] = $revision['rev'];
+      }
+    }
+    return $mapping;
+  }
+
+  protected function getTask(WorkspaceInterface $entity, $field_name) {
+    $task = new ReplicationTask();
+    $items = $entity->get($field_name);
+
+    if (!$items instanceof EntityReferenceFieldItemListInterface) {
+      throw new LogicException('Replication settings field does not exist.');
+    }
+
+    $referenced_entities = $items->referencedEntities();
+    if (count($referenced_entities) > 0) {
+      $task->setFilter($referenced_entities[0]->getFilterId());
+      $task->setParameters($referenced_entities[0]->getParameters());
+    }
+
+    return $task;
   }
 
 }
