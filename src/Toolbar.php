8eceefdfc0274fcab5f7d72dacc92aa75a77ee36
Andrei Jechiu <andrei.jechiu@gmail.com>
2019-04-15 15:32:14 +0300

    Change the message.

--- src/Toolbar.php
+++ src/Toolbar.php
@@ -158,7 +158,10 @@ class Toolbar {
         ->execute();
       if (!empty($update_in_queue)) {
         $this->messenger()
-          ->addWarning(t('A deployment to the active workspace is currently queued or in progress, you may not see all changes yet. Check @deployments_page for the status.', [
+          ->addWarning(t('Users are allowed to create only one push ' .
+            'and one pull (update) deployment. New deployments are allowed ' .
+            'only after the current queued deployments end (after cron run). ' .
+            'Check @deployments_page for the status.', [
             '@deployments_page' => Link::createFromRoute('Deployments page', 'entity.replication.collection')
               ->toString()
           ]));
